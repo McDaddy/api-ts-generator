cba8a49c4cde370a5173073e66960f64b5d19fa3
McDaddy <mcdaddychen@126.com>
2022-09-05 12:11:43 +0800

    feat: support url with ?

--- src/utils.ts
+++ src/utils.ts
@@ -26,20 +26,26 @@ export const extractPathParams = (path: string, params?: Record<string, unknown>
 
 /**
  * Fill in the actual value for the path with parameters by path-to-regexp
- * @param path Paths that may contain parameters, such as /fdp/:id/detail, path can not include `?`
+ * @param path Paths that may contain parameters, such as /fdp/:id/detail
  * @param params The incoming parameters may be query or params
  * @returns
  */
 export const generatePath = (path: string, params?: Record<string, unknown>) => {
   try {
     let urlPrefix = '';
+    let urlSuffix = '';
     const regex = new RegExp(HTTP_URL_REGEX);
     const matchResult = regex.exec(path);
     if (matchResult) {
       [urlPrefix] = matchResult;
     }
-    const toPathRepeated = compile(path.replace(HTTP_URL_REGEX, ''));
-    return `${urlPrefix}${toPathRepeated(params)}`;
+    const markIndex = path.indexOf('?');
+    if (markIndex > 0) {
+      urlSuffix = path.substring(markIndex);
+    }
+
+    const toPathRepeated = compile(path.replace(HTTP_URL_REGEX, '').replace(urlSuffix, ''));
+    return `${urlPrefix}${toPathRepeated(params)}${urlSuffix}`;
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('path:', path, 'Error parsing url parameters');
